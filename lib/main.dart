3780a1021025d64627f1f91cd0014707ba5c2bc7
ESMAT001 <esmat10.niazi@gmail.com>
2022-01-28 19:10:40 +0430

    row

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,135 @@ class MyApp extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.all(20.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  width: 100.0,
-                  child: Image.network(
-                      'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
-                )
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                    Container(
+                      width: 100.0,
+                      child: Image.network(
+                          'https://img-c.udemycdn.com/user/200_H/173002169_0cf4_2.jpg'),
+                    ),
+                  ],
+                ),
               ],
             ),
           )),
